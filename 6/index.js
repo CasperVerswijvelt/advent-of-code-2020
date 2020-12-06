3023e435eff9b72cf8b98ded99c942abea06471f
CasperVerswijvelt <casperverswijvelt@gmail.com>
2020-12-06 11:16:36 +0100

    Day 6 part 2

--- 6/index.js
+++ 6/index.js
@@ -7,21 +7,32 @@ let count = 0;
 
 lines.forEach((line) => {
   const yesMap = {};
-  console.log("Line");
+  //console.log("Line");
+
+  let individualCount = 0;
 
   persons = line.split("\r\n");
   persons.forEach((person) => {
     for (let i = 0; i < person.length; i++) {
       if (person[i] !== "\r") {
         yesMap[person[i]] =
-          typeof yesMap[person[i]] === "number" ? yesMap[person[i]] + 1 : 0;
+          typeof yesMap[person[i]] === "number" ? yesMap[person[i]] + 1 : 1;
       }
     }
   });
 
-  console.log(yesMap, "\n");
+  //console.log(yesMap, "\n");
 
-  count += Object.keys(yesMap).length;
+  let keys = Object.keys(yesMap);
+
+  for (let i = 0; i < keys.length; i++) {
+    if (yesMap[keys[i]] === persons.length) {
+      individualCount += 1;
+    }
+  }
+
+  count += individualCount;
+  console.log(`Everyone answerred yes to ${individualCount} questions`);
 });
 
 console.log(count);
