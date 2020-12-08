16b31f8b3dab712837dbdb2093c99e24606a0cfc
Casper Verswijvelt <casper.verswijvelt@basalte.be>
2020-12-08 13:39:40 +0100

    Day 8 part 2 not working yet

--- 8/index.js
+++ 8/index.js
@@ -19,15 +19,14 @@ while (true) {
   const split = line.split(" ");
   const instruction = split[0]
   const value = parseInt(split[1])
-  let done = false;
 
-  console.log(instruction, value)
+  console.log(`Instruction ${currentInstruction}: ${instruction}, ${value}`)
 
   lineExecutedMap[currentInstruction] = true;
 
   switch (instruction) {
     case "acc":
-      acc+=value
+      acc += value
       currentInstruction += 1;
       break
     case "jmp":
@@ -38,10 +37,79 @@ while (true) {
       break;
   }
 
-  if (done) {
+  if (currentInstruction === lines.length) {
+    console.log(`Program exited succesfully`)
     break;
   }
 }
 
-console.log(`Acc:  ${acc}`)
+console.log(`Acc:  ${acc}\n`)
+
+// Part 2
+
+console.log(`Acc 2: ${executeInstruction(0, false, 0, 0)} <= ACC INCORRECT`)
+
+function executeInstruction(originalInstruction, hasChanged, acc, level) {
+  if (originalInstruction === lines.length)
+    return acc;
+
+  if (level > lines.length)
+    return -1
+
+  const line = lines[originalInstruction];
+
+  if (!line)
+    return -1
+
+  const split = line.split(" ");
+  const instruction = split[0]
+  const value = parseInt(split[1])
+  let instructionNumber = originalInstruction
+  let changedInstruction = -1;
+
+  //console.log(`Instruction ${instructionNumber}: ${instruction}, ${value}, hasChanged ${hasChanged}`)
+
+  switch (instruction) {
+    case "acc":
+      acc += value;
+      instructionNumber += 1;
+      break;
+
+    case "jmp":
+
+      if (!hasChanged) {
+
+        changedInstruction = instructionNumber + 1
+      }
+
+      instructionNumber = instructionNumber + value
+
+      break;
+
+    case "nop":
+
+      if (!hasChanged) {
+
+        changedInstruction = instructionNumber + value
+      }
+
+      instructionNumber = instructionNumber + 1
+
+      break;
+  }
+
+  // First try actual instruction result
+  let result = executeInstruction(instructionNumber, hasChanged, acc, level + 1)
+
+  // If actual instruction resulted in loop, try alternative instruction result
+  if (result === -1 && changedInstruction !== -1) {
+    //console.log(`Trying alternative instruction number ${changedInstruction}`)
+    result = executeInstruction(changedInstruction, true, acc, level + 1)
+    if (result != -1) {
+      // THIS IS CORRECT, BUT RETURNED ACC ISN'T
+      console.log(`Changing instruction  ${originalInstruction} fixed it`)
+    }
+  }
+  return result
+}
 
