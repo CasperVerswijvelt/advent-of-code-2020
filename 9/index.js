da33da22f52a69dba1607a6c2329be1993745385
CasperVerswijvelt <casperverswijvelt@gmail.com>
2020-12-09 20:05:27 +0100

    Day 9 part 2

--- 9/index.js
+++ 9/index.js
@@ -6,6 +6,8 @@ const lines = text.split("\n");
 
 const preambleLength = parseInt(process.argv[3]);
 
+let invalidNumber = -1;
+
 for (let i = preambleLength; i < lines.length; i++) {
   const number = parseInt(lines[i]);
 
@@ -13,6 +15,34 @@ for (let i = preambleLength; i < lines.length; i++) {
     console.log(
       `Number ${number} at index ${i} is not the sum of 2 of the previous ${preambleLength} numbers`
     );
+    invalidNumber = number;
+    break;
+  }
+}
+
+for (let i = 0; i < lines.length; i++) {
+  let found = false;
+  let sum = 0;
+  let min = Number.MAX_SAFE_INTEGER,
+    max = Number.MIN_SAFE_INTEGER;
+  for (let j = i; j < lines.length; j++) {
+    const number = parseInt(lines[j]);
+
+    sum += number;
+
+    if (number > max) max = number;
+    if (number < min) min = number;
+
+    if (sum === invalidNumber) {
+      found = true;
+      console.log(
+        `Range: ${i}-${j}, Min: ${min}, Max: ${max}, Sum: ${min + max}`
+      );
+      break;
+    }
+  }
+
+  if (found) {
     break;
   }
 }
