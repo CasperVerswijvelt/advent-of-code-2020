4a8f812e12aa77d27e49ad35d2cf4ae081cfd297
CasperVerswijvelt <casperverswijvelt@gmail.com>
2020-12-15 21:24:24 +0100

    Day 15 part 2

--- 15/index.js
+++ 15/index.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 const text = fs.readFileSync(process.argv[2]).toString();
 const lines = text.split("\n");
 
+console.time("timer");
+
 for (let i = 0; i < lines.length; i++) {
   const numbers = lines[i].split(",").map((num) => parseInt(num));
   console.log(numbers);
@@ -12,20 +14,21 @@ for (let i = 0; i < lines.length; i++) {
 
   for (let k = 0; k < length; k++) {
     lastIndexMap[numbers[k]] = k;
-    console.log(`Number ${k + 1}: ${numbers[k]}`);
+    //console.log(`Number ${k + 1}: ${numbers[k]}`);
   }
   let lastNumber;
-  for (let k = numbers.length; k < 2020; k++) {
+  for (let k = numbers.length; k < 30000000; k++) {
     const previousNumber = numbers[k - 1];
     const lastIndex = lastIndexMap[previousNumber];
 
     lastNumber = typeof lastIndex === "number" ? k - 1 - lastIndex : 0;
     numbers[k] = lastNumber;
 
-    console.log(`Number ${k + 1}: ${numbers[k]}`);
+    //console.log(`Number ${k + 1}: ${numbers[k]}`);
 
     lastIndexMap[numbers[k - 1]] = k - 1;
   }
 
   console.log(lastNumber);
 }
+console.timeEnd("timer");
